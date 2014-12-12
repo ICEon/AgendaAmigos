846683574ba912f64a05db40272f47129c01984f
ICEon <matrix_eon@yahoo.com>
2014-12-12 08:00:07 -0600

    prueba 18

--- js/acciones.js
+++ js/acciones.js
@@ -22,8 +22,8 @@ if (DatosJSON.datos == 1)
 $('#resultados').empty();	 
 $('#ocultar').show();	 
 	for (var i = 0; i <DatosJSON.amigos.length  ; i++) {
-
-$('#resultados').append("<div style='width:100%; font-size:1em; font-weight:bold;' class='foto'><img src='http://192.168.1.30/practica12/"+DatosJSON.amigos[i].IdAmigo+".jpg' class='famigo' id='"+DatosJSON.amigos[i].IdAmigo+"' style='vertical-align:middle;'><a href='#' class='detalle' data-role='button'>"+DatosJSON.amigos[i].Nombre + " "+ DatosJSON.amigos[i].ApPaterno + " " + DatosJSON.amigos[i].ApMaterno +"</a><hr></div> ");
+//<img src='http://192.168.1.30/practica12/"+DatosJSON.amigos[i].IdAmigo+".jpg' class='famigo' id='"+DatosJSON.amigos[i].IdAmigo+"' style='vertical-align:middle;'>
+$('#resultados').append("<div style='width:100%; font-size:1em; font-weight:bold;' class='foto'><a href='#' class='detalle' data-role='button'>"+DatosJSON.amigos[i].Nombre + " "+ DatosJSON.amigos[i].ApPaterno + " " + DatosJSON.amigos[i].ApMaterno +"</a><hr></div> ");
 
 }
 $('.famigo').width($('.foto').width()/5);
