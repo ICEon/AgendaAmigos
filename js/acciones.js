e439db5452a1f981f50b863391ce724c987a30e2
ICEon <matrix_eon@yahoo.com>
2014-12-12 08:12:43 -0600

    Prueba 21

--- js/acciones.js
+++ js/acciones.js
@@ -23,7 +23,8 @@ $('#resultados').empty();
 $('#ocultar').show();	 
 	for (var i = 0; i <DatosJSON.amigos.length  ; i++) {
 //<img src='http://192.168.1.30/practica12/"+DatosJSON.amigos[i].IdAmigo+".jpg' class='famigo' id='"+DatosJSON.amigos[i].IdAmigo+"' style='vertical-align:middle;'>
-$('#resultados').append('<div style="width:100%; font-size:1em; font-weight:bold;" class="foto"><a href="#" class="detalle" data-role="button">'+DatosJSON.amigos[i].Nombre + ' '+ DatosJSON.amigos[i].ApPaterno + ' ' + DatosJSON.amigos[i].ApMaterno +'</a><hr></div>');
+//<div style="width:100%; font-size:1em; font-weight:bold;" class="foto"><a href="#" class="detalle" data-role="button">'+DatosJSON.amigos[i].Nombre + ' '+ DatosJSON.amigos[i].ApPaterno + ' ' + DatosJSON.amigos[i].ApMaterno +'</a><hr></div>
+$('#resultados').append('<a href="#" data-role="button">Prueba</a>');
 
 }
 $('.famigo').width($('.foto').width()/5);
